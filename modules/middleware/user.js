5c4141a2650f42c580737d296e01ca2d66b09b71
paulhan <zhangyu.han@tcl.com>
2017-04-29 09:20:19 +0800

    add md5

--- modules/middleware/user.js
+++ modules/middleware/user.js
@@ -3,12 +3,13 @@
  * 中间层做一些数据的转换，及预处理，消化系统内部错误
  * @type {*|exports|module.exports}
  */
-var Promise = require("bluebird");
-var async = require('async');
-var userSql = require('../dao/user');
-var searchSql = require('../dao/search');
-let i18n = require('../i18n');
-let consts = require('../util/const');
+ var crypto=require('crypto');
+ var Promise = require("bluebird");
+ var async = require('async');
+ var userSql = require('../dao/user');
+ var searchSql = require('../dao/search');
+ let i18n = require('../i18n');
+ let consts = require('../util/const');
 
 // 防止SQL注入攻击
 var isValid = (email, password)=>(/^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/).test(email) && (/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/).test(password);
@@ -20,7 +21,10 @@ var user = {};
 user.userCreate = (data, locale)=> {
     return new Promise((resolve, reject) => {
         let {email,PW}=data,
-            date = new Date();
+        date = new Date();
+        var hasher=crypto.createHash("md5");
+        hasher.update(PW);
+        PW=hasher.digest('hex');
         let L = i18n.getLocale(locale);
         // 先验证是否存在再插入，已存在不返回ID号，成功返回ID号
         user.userExist(data).then(()=> {
@@ -29,9 +33,9 @@ user.userCreate = (data, locale)=> {
             if (err === undefined) {// 没查到用户，先插入初始推荐的参数
                 let user;
                 userSql.add(email, PW, date)
-                    .then(result=> {
-                        user = result;
-                        let asyncArray = [];
+                .then(result=> {
+                    user = result;
+                    let asyncArray = [];
                         // FIXME 这里的两成的异步异步嵌报warning
                         // (node:12436) Warning: a promise was created in a handler at ...  user.js:49:31
                         // TODO 找到没有返回的那个Promise对象
@@ -39,35 +43,35 @@ user.userCreate = (data, locale)=> {
                         consts.defaultSearchData(locale).searchList.forEach(val=> {
                             asyncArray.push((callback)=> {
                                 searchSql.add({
-                                        uId: user.id,
-                                        name: val.name,
-                                        link: val.link,
-                                        open: val.open,
-                                        hide: val.hide,
-                                        img: val.img
-                                    })
-                                    .then(result=> {
-                                        callback(null, result);
-                                        return null;
-                                    }).catch(err=>reject(L.unKnowError))
+                                    uId: user.id,
+                                    name: val.name,
+                                    link: val.link,
+                                    open: val.open,
+                                    hide: val.hide,
+                                    img: val.img
+                                })
+                                .then(result=> {
+                                    callback(null, result);
+                                    return null;
+                                }).catch(err=>reject(L.unKnowError))
                             })
                         });
                         async.series(asyncArray, function (err, results) {
                             let currentSearch = results[0]['last_insert_id()'];
                             // 获取插入的第一个推荐search的ID号results[0]，写入user表
                             userSql.updateCurrentSearch(user.id, currentSearch)
-                                .then(()=> {
-                                    resolve({
-                                        userId: user.id,
-                                        name: user.username,
-                                        img: user.image,
-                                        email: user.email
-                                    });
-                                    return null;
-                                }).catch(err=>reject(L.unKnowError))
+                            .then(()=> {
+                                resolve({
+                                    userId: user.id,
+                                    name: user.username,
+                                    img: user.image,
+                                    email: user.email
+                                });
+                                return null;
+                            }).catch(err=>reject(L.unKnowError))
                         });
                     })
-                    .catch(err=>reject(L.unKnowError))
+                .catch(err=>reject(L.unKnowError))
             } else {
                 reject(L.unKnowError)
             }
@@ -86,6 +90,9 @@ user.userExist = (data, comparePW)=> {
                 email: result.email
             };
             if (comparePW) {
+                var hasher=crypto.createHash("md5");
+                hasher.update(PW);
+                PW=hasher.digest('hex');
                 let validTime = (new Date() - new Date(result.randomDate)) / (1000 * 60 * 60) < 1;
                 if (result.password == PW || (validTime && result.randomPW == PW)) {// 登录密码正确或者检查random密码是否在有效期内匹配
                     resolve(user)
@@ -123,8 +130,8 @@ user.updateRPW = (data, locale)=> {
             PW: vaildPW
         }).then(()=> {
             userSql.updateRPW(data.email, data.rPW, rDate)
-                .then(()=> resolve(true))
-                .catch(()=> reject(L.unKnowError));
+            .then(()=> resolve(true))
+            .catch(()=> reject(L.unKnowError));
             return null;
         }).catch(()=> {
             reject(L.emailNotExist)
@@ -135,11 +142,17 @@ user.updateRPW = (data, locale)=> {
 user.updatePW = (data)=> {
     return new Promise((resolve, reject) => {
         let PW = data.PW;
+        var hasher=crypto.createHash("md5");
+        hasher.update(data.oldPW);
+        let oldPW=hasher.digest('hex');
         userSql.queryById(data.userID).then((result)=> {
-            if (result.password == data.oldPW && isValid(vaildEmail, PW)) {
+            if (result.password == oldPW && isValid(vaildEmail, PW)) {
+                hasher=crypto.createHash("md5");
+                hasher.update(PW);
+                PW=hasher.digest('hex');
                 userSql.updatePW(data.userID, PW)
-                    .then(()=> resolve(true))
-                    .catch(()=>reject(new Error()));
+                .then(()=> resolve(true))
+                .catch(()=>reject(new Error()));
             } else {
                 reject(new Error())
             }
@@ -151,24 +164,24 @@ user.updatePW = (data)=> {
 user.updateUsername = (data)=> {
     return new Promise((resolve, reject) => {
         userSql.updateUsername(data.userID, data.username)
-            .then((result)=>resolve(true))
-            .catch(()=>reject(new Error()))
+        .then((result)=>resolve(true))
+        .catch(()=>reject(new Error()))
 
     })
 };
 user.updateAvatar = (data)=> {
     return new Promise((resolve, reject) => {
         userSql.updateAvatar(data.userID, data.imgSrc)
-            .then((result)=>resolve(true))
-            .catch(()=>reject(new Error()))
+        .then((result)=>resolve(true))
+        .catch(()=>reject(new Error()))
 
     })
 };
 user.updateCurrentSearch = (data)=> {
     return new Promise((resolve, reject) => {
         userSql.updateCurrentSearch(data.userID, data.id)
-            .then((result)=>resolve(true))
-            .catch(()=>reject(new Error()))
+        .then((result)=>resolve(true))
+        .catch(()=>reject(new Error()))
 
     })
 };
